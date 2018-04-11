06c856651669a218b31735058e57cf6bb08a04c6
Lance Laughlin <lancealaughlin@gmail.com>
2018-04-10 21:15:13 -0400

    unit test complete

--- src/components/product-item/product-item.js
+++ src/components/product-item/product-item.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
 
 class ProductItem extends Component {
-  state = {}
+
+  static defaultProps = {
+    productData: {}
+  }
+  
+  state = {
+    quantity: "",
+    disabled: false,
+  }
+
+  handleChange = (event) => {
+    const quantity = event.target.value;
+    if(quantity > this.props.productData.available){
+      this.setState({disabled: true});
+    }
+    this.setState({quantity: event.target.value});
+  }
+
+  maybeRenderError = () => {
+    if(this.state.disabled) {
+      return(
+        <p className="error-message">
+          Sorry, we only have {this.props.productData.available} in stock
+        </p>
+      );
+    }
+  }
+
   render() { 
     return (
       <div>
-        Product
+        <h2 className="product-title">Product Name</h2>
+        <img className="product-image" />
+        <form className="product-form">
+          <input 
+            className="quantity-input" 
+            value={this.state.quantity} 
+            onChange={this.handleChange} />
+            <button 
+            className="add-to-cart"
+            disabled={this.state.disabled}>
+              Add To Cart
+            </button>
+            {this.maybeRenderError()}
+        </form>
       </div>
     )
   }
--- src/components/product-item/product-item.test.js
+++ src/components/product-item/product-item.test.js
@@ -1,20 +1,44 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import ProductItem from './product-item';
 
+const wrapper = shallow(<ProductItem />);
 describe('Product Item', () => {
 
   describe('Child components render', () => {
-    it('should render the product title');
-    it('should render the product image');
-    it('should render the purchase form');
+    it('should render the product title', () => {
+      expect(wrapper.find('.product-title').length).toBe(1);
+    });
+    it('should render the product image', () => {
+      expect(wrapper.find(".product-image").length).toBe(1);
+    });
+    it('should render the purchase form', () => {
+      expect(wrapper.find(".product-form").length).toBe(1);
+    });
   });
 
   describe('Product Form', () => {
     it("should initialize state to empty string");
     it('should update state on input change event');
     it('should disable button on empty field');
-    it('should disable button when input is more than available');
-    it("should show message when input is more than available");
+
+    describe('User enters more than available quantity', () => {
+      let wrapper;
+      beforeEach(() => {
+        const productData = { name: "Fresh Prince: Season 2", price: 10, available: 6 };
+        wrapper = shallow(<ProductItem productData={productData} />);
+      })
+
+      it("should disable button when input is more than available", () => {
+        wrapper.find(".quantity-input").simulate("change", { target: { value: "8" } });
+        expect(wrapper.find(".add-to-cart").prop("disabled")).toBe(true);
+      });
+
+      it("should show message when input is more than available", () => {
+        wrapper.find(".quantity-input").simulate("change", { target: { value: "8" } });
+        expect(wrapper.find(".error-message").text()).toBe("Sorry, we only have 6 in stock");
+      });
+    });
   });
 
 });
